019882d63d5815bb5227cecd828dca5dd4e93eca
John <souzajohnlee@gmail.com>
2021-09-21 16:43:14 -0700

    set up router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import FormsRouteContainer from "./components/FormsRouteContainer.tsx/FormsRouteContainer";
+import Header from "./components/Header/Header";
+import HomePage from "./components/HomePage/HomePage";
+
 function App() {
   return (
-    <div>
-      Hello world
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/forms">
+          <FormsRouteContainer />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/FormsRouteContainer.tsx/FormsRouteContainer.tsx
@@ -0,0 +1,20 @@
+import { Switch, Route } from "react-router-dom";
+import FormsIndexPage from "../FormsIndexPage/FormsIndexPage";
+import FormsNewPage from "../FormsNewPage.tsx/FormsNewPage";
+import FormsShowPage from "../FormsShowPage/FormsShowPage";
+
+export default function FormsRouteContainer() {
+  return (
+    <Switch>
+      <Route exact path="/forms">
+        <FormsIndexPage />
+      </Route>
+      <Route path="/forms/new">
+        <FormsNewPage />
+      </Route>
+      <Route path="/forms/:id">
+        <FormsShowPage />
+      </Route>
+    </Switch>
+  );
+}
