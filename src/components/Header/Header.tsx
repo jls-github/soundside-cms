8e16f350f69b6a3afa365461fab05451fc44a536
John <souzajohnlee@gmail.com>
2021-10-02 01:02:48 -0700

    style navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,25 @@
-import {NavLink} from 'react-router-dom'
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-    return (
-        <header>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/forms">Forms</NavLink>
-        </header>
-    )
+  return (
+    <Navbar className="bg-light">
+      <Container>
+        <div className="col-4 offset-4 text-center">
+          <Navbar.Brand>Soundside CMS</Navbar.Brand>
+        </div>
+        <Nav className="col-4">
+          <div className="ms-auto">
+            <Link className="nav-item mx-1" to="/">
+              Home
+            </Link>
+
+            <Link className="nav-item mx-1" to="/forms">
+              Forms
+            </Link>
+          </div>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 }
