322315d200aa63fe654cdb5a1f4052c43d281f98
John <souzajohnlee@gmail.com>
2021-10-01 20:50:16 -0700

    fetch individual forms

--- src/components/FormsShowPage/FormsShowPage.tsx
+++ src/components/FormsShowPage/FormsShowPage.tsx
@@ -1,9 +1,49 @@
-export default function FormsShowPage() {
-    // View, edit, or delete a form here
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { FORMS_URL } from "../../constraints/urls";
+import IForm from "../../types/form";
 
-    return (
-        <div>
-            Forms Show Page
-        </div>
-    )
+export default function FormsShowPage(): JSX.Element {
+  const { id } = useParams<{ id: string }>();
+  const [form, setForm] = useState<IForm | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    async function fetchForm() {
+      const res = await fetch(`${FORMS_URL}/${id}`, {
+        headers: { Authorization: "test_password" },
+      });
+      if (res.ok) {
+        const json = await res.json();
+        setForm(json);
+      } else {
+        console.log(res);
+      }
+      setLoading(false);
+    }
+
+    fetchForm()
+  }, [id]);
+
+  if (loading) return <div>loading...</div>;
+  if (!form) return <div>Something went wrong... Please try again later.</div>;
+
+  return (
+    <div>
+      <p>Name: {form.name}</p>
+      <p>Guest: {form.guest.toString()}</p>
+      <div>
+        <h3>Inputs: </h3>
+        {form.inputs?.map((input, idx) => {
+          return (
+            <div key={`input-${idx}`}>
+              <p>Name: {input.name}</p>
+              <p>Label Text: {input.labelText}</p>
+              <p>Type: {input.type}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
