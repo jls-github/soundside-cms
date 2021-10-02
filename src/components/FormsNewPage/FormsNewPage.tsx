6b674267d14f6faa3d7da73dc8cd0eeee153a055
John <souzajohnlee@gmail.com>
2021-10-01 21:31:43 -0700

    set up form

--- src/components/FormsNewPage/FormsNewPage.tsx
+++ src/components/FormsNewPage/FormsNewPage.tsx
@@ -1,9 +1,48 @@
-export default function FormsNewPage() {
-    // Create new forms here
+import { useState } from "react";
+import IForm from "../../types/form";
+import { InputEnum } from "../../types/input";
 
-    return (
+export default function FormsNewPage(): JSX.Element {
+  const [form, setForm] = useState<IForm>(initialFormData);
+
+  return (
+    <div>
+      <form>
+        <label>Form Name: </label>
+        <input value={form.name} />
+        <br />
+        <label>Guest Form?</label>
+        <input type="checkbox" checked={form.guest} />
+        <input type="checkbox" checked={!form.guest} />
+        <br />
         <div>
-            Forms New Page
+          <h3>Inputs</h3>
+
+          {form.inputs?.map((input, idx) => (
+            <div key={`input-${idx}`}>
+              <label>Input Name: </label>
+              <input value={input.name} />
+              <br />
+              <label>Input Label Text: </label>
+              <input value={input.labelText} />
+              <br />
+              <label>type</label>
+              <select value={input.type}>
+                  <option value="text">text</option>
+                  <option value="textarea">textarea</option>
+                  <option value="checkbox">checkbox</option>
+                  <option value="number">number</option>
+              </select>
+            </div>
+          ))}
         </div>
-    )
+      </form>
+    </div>
+  );
 }
+
+const initialFormData: IForm = {
+  name: "",
+  guest: true,
+  inputs: [{ labelText: "", name: "", type: InputEnum["text"] }],
+};
--- src/types/input.ts
+++ src/types/input.ts
@@ -1,7 +1,11 @@
 interface IInput {
     labelText: string,
     name: string,
-    type: string
+    type: InputEnum
+}
+
+export enum InputEnum {
+    checkbox = "checkbox", text = "text", textarea = "textarea", number = "number"
 }
 
 export default IInput
