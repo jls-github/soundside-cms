7beda5c06c17802471eb77ec890f3d47e52b34d1
John <souzajohnlee@gmail.com>
2021-10-02 01:17:10 -0700

    use useFect hook on show page

--- src/components/FormsShowPage/FormsShowPage.tsx
+++ src/components/FormsShowPage/FormsShowPage.tsx
@@ -1,30 +1,13 @@
-import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { FORMS_URL } from "../../constraints/urls";
+import useFetch from "../../hooks/useFetch";
 import IForm from "../../types/form";
 import LoadingSpinner from "../shared/LoadingSpinner";
 
 export default function FormsShowPage(): JSX.Element {
   const { id } = useParams<{ id: string }>();
-  const [form, setForm] = useState<IForm | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
 
-  useEffect(() => {
-    async function fetchForm() {
-      const res = await fetch(`${FORMS_URL}/${id}`, {
-        headers: { Authorization: "test_password" },
-      });
-      if (res.ok) {
-        const json = await res.json();
-        setForm(json);
-      } else {
-        console.log(res);
-      }
-      setLoading(false);
-    }
-
-    fetchForm()
-  }, [id]);
+  const { data: form, loading } = useFetch<IForm>(`${FORMS_URL}/${id}`);
 
   if (loading) return <LoadingSpinner />;
   if (!form) return <div>Something went wrong... Please try again later.</div>;
