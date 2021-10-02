c5291c62382d3a075c9b00ab349766e08e3c1534
John <souzajohnlee@gmail.com>
2021-10-01 20:30:56 -0700

    fetch forms

--- src/components/FormsIndexPage/FormsIndexPage.tsx
+++ src/components/FormsIndexPage/FormsIndexPage.tsx
@@ -4,26 +4,37 @@ import IForm from "../../types/form";
 
 export default function FormsIndexPage(): JSX.Element {
   const [forms, setForms] = useState<IForm[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     async function fetchForms(): Promise<void> {
       const res = await fetch(FORMS_URL, {
         headers: { Authorization: "test_password" },
       });
-      const json = await res.json();
-      setForms(json);
+      if (res.ok) {
+        const json: IForm[] = await res.json();
+        setForms(json);
+      } else {
+        console.log(res);
+      }
+
+      setLoading(false);
     }
     fetchForms();
   }, []);
 
-  if (!forms) {
+  if (loading) {
     return <div>Loading...</div>;
+  }
+
+  if (!forms) {
+    return <div>Something went wrong... Please try again later</div>;
   }
 
   return (
     <div>
-      {forms.map((form) => {
-        return <p>{form.name}</p>;
+      {forms.map((form, idx) => {
+        return <p key={`form-${idx}`}>{form.name}</p>;
       })}
     </div>
   );
