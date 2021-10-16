7f4aab2f199c1b96c0e59be50e63cafe905320f2
John <souzajohnlee@gmail.com>
2021-10-16 16:28:18 -0700

    add warning for old inputs

--- src/components/shared/InputDropDown.tsx
+++ src/components/shared/InputDropDown.tsx
@@ -1,4 +1,4 @@
-import { FormControl, FormText } from "react-bootstrap";
+import { FormControl, FormText, Alert } from "react-bootstrap";
 import IInput from "../../types/input";
 
 interface InputDropDownProps {
@@ -14,6 +14,7 @@ export default function InputDropDown({
 }: InputDropDownProps): JSX.Element {
   return (
     <>
+    <Alert variant="warning">Warning: Changing a question that already exists will change that question for ALL FORMS! Select "New Question" if you want to your changes to apply to a question for only this form.</Alert>
     <FormControl as="select" value={selectedInput} onChange={handleChangeSelectedInput}>
       <option value="New Input">New Question</option>
       {inputs.map((input, idx) => (
