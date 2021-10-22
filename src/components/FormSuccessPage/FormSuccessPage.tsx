fe390477ac94b64e027215c25361f85e0c096b05
John <souzajohnlee@gmail.com>
2021-10-22 15:04:48 -0700

    Provide link on successful form creation

--- src/components/FormSuccessPage/FormSuccessPage.tsx
+++ src/components/FormSuccessPage/FormSuccessPage.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { useLocation } from "react-router-dom";
+import IForm from "../../types/form";
+import PageWrapper from "../shared/PageWrapper";
 
 export default function FormSuccessPage() {
+  const location = useLocation<IForm>();
+
+  const formId = location.state?.id;
+
+  if (formId)
     return (
-        <div>
-            Form submitted successfully!
-        </div>
-    )
+      <PageWrapper>
+        <h2>Form Submitted Successfully!</h2>
+        <p>
+          You can view your new form at url:{" "}
+          <a href={`https://soundsideforms.netlify.app/forms/${formId}`}>
+            https://soundsideforms.netlify.app/forms/{formId}
+          </a>
+        </p>
+        <p>This QR Code will give you access to above url</p>
+      </PageWrapper>
+    );
+
+  return <div>Form submitted successfully!</div>;
 }
